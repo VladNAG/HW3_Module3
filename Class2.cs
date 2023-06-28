b38810f1d2192c012e662d83b9fef3ede2c6c41d
Vladyslav Nahornyi <vladnagornyj2@gmail.com>
2023-06-28 19:09:46 +0100

    Solved

---
+++ Class2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3_Module3
+{
+    internal class Class2
+    {
+        private static int result = 0;
+        public delegate int MyDelegatePow(int x, int y);
+        public delegate bool MyDelegatResult(int x);
+        public static MyDelegatResult Calc(MyDelegatePow pow, int x, int y)
+        {
+            result = pow(x, y);
+            MyDelegatResult res = Result;
+            return res;
+        }
+
+        public static bool Result(int z)
+        {
+            if (result % z == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace HW3_Module3
+﻿using static HW3_Module3.Class1;
+using static HW3_Module3.Class2;
+
+namespace HW3_Module3
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MyDelegateShow show = Show;
+            MyDelegatePow pow = Class1.Pow;
+            var v = Class2.Calc(pow, 10, 5);
+            show(v(2));
+        }
+
+        public static void Show(bool x)
+        {
+            Console.WriteLine(x);
         }
     }
 }
